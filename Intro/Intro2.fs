1da4db345fb9f1c221c9116f910d2615f3ec117d
Anders0106 <andersgfh@gmail.com>
2025-08-28 12:32:36 +0200

    1.2.3

--- Intro/Intro2.fs
+++ Intro/Intro2.fs
@@ -32,13 +32,22 @@ let e5 = Mul(Var "2", Sub(Var "v", Add(Var "W", Var "Z")));;
 
 let e6 = Add(Add(Var "x", Var "y"), Add(Var "z", Var "v"));;
 
+let rec fmt e : string =
+    match e with
+    |CstI i -> string i 
+    |Var x -> x
+    | Add(e1, e2) -> "(" + fmt e1 + " + " + fmt e2 + ")"
+    | Sub(e1, e2) -> "(" + fmt e1 + " - " + fmt e2 + ")"
+    | Mul(e1, e2) -> "(" + fmt e1 + " * " + fmt e2 + ")"
+    
+let e12 =  fmt e4   
+    
 type expr = 
   | CstI of int
   | Var of string
   | Prim of string * expr * expr
   | If of expr * expr * expr;;
   
-
 let e1 = CstI 17;;
 
 let e2 = Prim("+", CstI 3, Var "a");;
@@ -71,12 +80,12 @@ let e2v2 = eval e2 [("a", 314)];;
 let e3v  = eval e3 env;;
 
 // Tests
-let e4 = eval (Prim("max", CstI 3, CstI 8)) env;; // Test max
+let e7 = eval (Prim("max", CstI 3, CstI 8)) env;; // Test max
 
-let e5 = eval (Prim("min", CstI 3, CstI 8)) env;; // Test min
+let e8 = eval (Prim("min", CstI 3, CstI 8)) env;; // Test min
 
-let e6 = eval (Prim("==", CstI 2, Var "a")) env;; // Test equals
+let e9 = eval (Prim("==", CstI 2, Var "a")) env;; // Test equals
 
-let e7 = eval (If(Var "a", CstI 11, CstI 22)) env;; // Test If
+let e10 = eval (If(Var "a", CstI 11, CstI 22)) env;; // Test If
 
-let e8 = eval (If(CstI 0, CstI 11, CstI 22)) env;; // Test If
+let e11 = eval (If(CstI 0, CstI 11, CstI 22)) env;; // Test If
